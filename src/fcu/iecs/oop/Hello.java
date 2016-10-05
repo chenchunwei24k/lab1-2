44e28192784f10d86e30a52cf82744cefa59b377
fcu-D0440864 <chenchunwei24k@gmail.com>
2016-10-05 19:37:35 +0800

    it 變 "her" Cannot 變 Can't

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -3,7 +3,7 @@ package fcu.iecs.oop;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Let it go! Let it go! Cannot hold it back anymore");
+		System.out.println("Let \"her\" go! Let \"her\" go! Can't hold \"her\" back anymore");
 	}
 
 }
